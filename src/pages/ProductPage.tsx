1622fd6d6c217e14f8326e818d311886c9360790
azevedocti <136394814+azevedocti@users.noreply.github.com>
2023-11-11 00:47:45 +0000

    ajsasja

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -22,41 +22,46 @@ export function ProductPage() {
 
           <div>
             <Link to="/usuario">
-              <img id="back" src="/icons/seta-esquerda.png" alt="Voltar"/>
+              <img id="back" src="/icons/seta-esquerda.png" alt="Voltar" />
             </Link>
           </div>
 
-          <div id="titulo">
-            <h1 className="NeonText">Caipirinha de Limão {id}</h1>
-            <h2 className="NeonText">Cachaça, Limão, açúcar e MUITO gelo.</h2>
+          <div style={{ display: "flex" }}>
+
+            <div id="produto" style={{flex:1}}>
+              <img id="caipirinha" src="/imgs/caipirinha.jpeg" alt="Caipirinha" />
+            </div>
+            <div style={{flex:1}}>
+              <div id="titulo">
+                <h1 className="NeonText">Caipirinha de Limão {id}</h1>
+                <h2 className="NeonText">Cachaça, Limão, açúcar e MUITO gelo.</h2>
+              </div>
+
+              <div id="descricao">
+                <h2 id="desc">
+                  Ingredientes:
+                  <ul>
+                    <li>1 limão</li>
+                    <li>2 colheres de chá de açúcar</li>
+                    <li>Gelo a gosto</li>
+                    <li>50 ml de cachaça</li>
+                  </ul>
+                  Instruções:
+                  <ol>
+                    <li>Corte o limão em pedaços ou fatias.</li>
+                    <li>Coloque o limão cortado em um copo.</li>
+                    <li>Adicione o açúcar sobre o limão.</li>
+                    <li>Use um socador para amassar o limão e misturar bem com o açúcar.</li>
+                    <li>Adicione gelo a gosto no copo.</li>
+                    <li>Despeje a cachaça sobre a mistura.</li>
+                    <li>Mexa bem. Sirva e aproveite sua caipirinha de limão!</li>
+                  </ol>
+                </h2>
+              </div>
             </div>
 
-            <div id="descricao">
-            <h2 id="desc">
-              Ingredientes:
-              <ul>
-                <li>1 limão</li>
-                <li>2 colheres de chá de açúcar</li>
-                <li>Gelo a gosto</li>
-                <li>50 ml de cachaça</li>
-              </ul>
-              Instruções:
-              <ol>
-                <li>Corte o limão em pedaços ou fatias.</li>
-                <li>Coloque o limão cortado em um copo.</li>
-                <li>Adicione o açúcar sobre o limão.</li>
-                <li>Use um socador para amassar o limão e misturar bem com o açúcar.</li>
-                <li>Adicione gelo a gosto no copo.</li>
-                <li>Despeje a cachaça sobre a mistura.</li>
-                <li>Mexa bem. Sirva e aproveite sua caipirinha de limão!</li>
-              </ol>
-            </h2>
-            </div>
-      
-
-          <div id="produto">
-            <img id="caipirinha" src="/imgs/caipirinha.jpeg" alt="Caipirinha"/>
           </div>
+
         </div>
       </div>
     </>
