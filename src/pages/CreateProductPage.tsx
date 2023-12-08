bbf829c01f63c34e7401c76e0ddfe6b67508e2ff
azevedocti <136394814+azevedocti@users.noreply.github.com>
2023-12-08 23:12:53 +0000

    asjnansajs

--- src/pages/CreateProductPage.tsx
+++ src/pages/CreateProductPage.tsx
@@ -3,10 +3,11 @@ import { Link, useNavigate } from 'react-router-dom';
 import { addItem } from '/workspaces/MixologyGram/src/services/firebase';  // Substitua pelo caminho correto
 
 export function CreateProductPage() {
-  const [titulo, setTitulo] = useState('');
-  const [subtitulo, setSubtitulo] = useState('');
-  const [foto, setFoto] = useState('');
-  const [descricao, setDescricao] = useState('');
+  const [id, setId] = useState('');
+  const [title, setTitulo] = useState('');
+  const [subtitle, setSubtitulo] = useState('');
+  const [image, setFoto] = useState('');
+  const [description, setDescricao] = useState('');
   const nav = useNavigate();
 
   async function handleLogin(e: FormEvent) {
@@ -14,15 +15,15 @@ export function CreateProductPage() {
 
     // Crie um objeto com os dados do produto
     const produtoData = {
-      titulo,
-      subtitulo,
-      foto,
-      descricao,
+      title,
+      subtitle,
+      image,
+      description,
     };
 
     try {
       // Chame a função addItem para adicionar o produto ao banco de dados
-      await addItem('suaColecao', 'seuDocumento', produtoData);
+      await addItem('drinks', id, produtoData);
 
       // Após adicionar com sucesso, redirecione para a página de feed
       alert('Produto postado com sucesso!');
@@ -45,29 +46,33 @@ export function CreateProductPage() {
       </div>
 
       <form onSubmit={handleLogin}>
+      <div>
+          <label>ID: </label>
+          <input onChange={(e) => setId(e.target.value)} value={id} />
+        </div>
         <div>
           <label>Título: </label>
-          <input onChange={(e) => setTitulo(e.target.value)} value={titulo} />
+          <input onChange={(e) => setTitulo(e.target.value)} value={title} />
         </div>
 
         <div>
           <label>Principais Ingredientes: </label>
           <input
             onChange={(e) => setSubtitulo(e.target.value)}
-            value={subtitulo}
+            value={subtitle}
           />
         </div>
 
         <div>
           <label>Foto: </label>
-          <input onChange={(e) => setFoto(e.target.value)} value={foto} />
+          <input onChange={(e) => setFoto(e.target.value)} value={image} />
         </div>
 
         <div>
           <label>Descrição: </label>
           <textarea
             onChange={(e) => setDescricao(e.target.value)}
-            value={descricao}
+            value={description}
           />
         </div>
 
